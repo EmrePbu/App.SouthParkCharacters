aeeb40676764b165598ccbcbd381cac46bdb56b6
emrep <emrewindowsarg@gmail.com>
2022-08-12 19:30:40 +0300

    Add images and viewBinding property.

--- app/src/main/java/com/emrepbu/appsouthparkcharacters/DetailsActivity.kt
+++ app/src/main/java/com/emrepbu/appsouthparkcharacters/DetailsActivity.kt
@@ -2,10 +2,16 @@ package com.emrepbu.appsouthparkcharacters
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.emrepbu.appsouthparkcharacters.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
--- app/src/main/java/com/emrepbu/appsouthparkcharacters/MainActivity.kt
+++ app/src/main/java/com/emrepbu/appsouthparkcharacters/MainActivity.kt
@@ -2,10 +2,16 @@ package com.emrepbu.appsouthparkcharacters
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.emrepbu.appsouthparkcharacters.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
